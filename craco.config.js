7b2df784d7b362418359ab766a9367e6a2d36e40
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2022-12-18 21:29:53 +0530

    craco prod

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  webpack: {
-      configure: (webpackConfig) => {
-          webpackConfig.externals = {
-              'Config': JSON.stringify(process.env.NODE_ENV === 'production' ? require('./config.prod.json') : require('./conf/config.dev.json'))
-          };
-          return webpackConfig;
+  reactScriptsVersion: "react-scripts",
+  style: {
+    css: {
+      loaderOptions: () => {
+        return {
+          url: false,
+        };
       },
-  }
+    },
+  },  webpack: {
+    configure: (webpackConfig) => {
+        webpackConfig.externals = {
+            'Config': JSON.stringify(process.env.NODE_ENV === 'production' ? require('./config.prod.json') : require('./conf/config.dev.json'))
+        };
+        return webpackConfig;
+    },
+}
 };
