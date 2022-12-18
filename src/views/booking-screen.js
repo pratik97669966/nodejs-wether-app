12675be7530381207683a28348b039a2b8aa002e
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2022-12-18 20:50:28 +0530

    done

--- src/views/booking-screen.js
+++ src/views/booking-screen.js
@@ -3,10 +3,56 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 import './booking-screen.css'
-
 const BookingScreen = (props) => {
+var retrievedObject = localStorage.getItem('UserData');
+const [doctor, setDoctor] = React.useState("");
+var userData = JSON.parse(retrievedObject);
   var drId =localStorage.getItem('drId');
   var drName =localStorage.getItem('drName');
+  var specality =localStorage.getItem('specality');
+  var photourl="http://68.183.83.230:8765/provider-service/doctor/" + drId+ "/photo"
+ 
+  const getDoctordetailsById = async () => {
+      const payload = {
+      firstName: finalFirstName,
+      lastName: finalLastName,
+      mobileNumber: mobileNumber,
+      requestedRole:"PATIENT",
+      preferredLanguageId:"5ad887231fd47a000d03368d"
+      }
+      const responce = await axios({
+        method: "post",
+        // url: `https://api.qupdev.com/provider-service/doctor/by-id/`+drId,
+        url: `http://68.183.83.230:8765/provider-service/doctor/by-id/`+drId,
+        data: payload,
+        headers: {
+          "Content-Type": "application/json",
+           'Authorization': 'Basic '+btoa('qup-mobile:mob@46$qup'),
+        },
+      }).then((responce) => {      
+        setDoctor(responce.data)
+      }).catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          // console.log(error.response.data);
+          // console.log("Error api status",error.response.status);
+          console.log("ERROR",error.responce)
+          alert("Something Went Wrong Try Again")
+        
+          // console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
+  };
   return (
     <div className="booking-screen-container">
       <Helmet>
@@ -24,13 +70,13 @@ const BookingScreen = (props) => {
                 <div className="booking-screen-author">
                   <img
                     alt="image"
-                    src="https://max-website20-images.s3.ap-south-1.amazonaws.com/Dr_Puneet_sq_3be6907e0c.jpg"
+                    src={photourl}
                     className="booking-screen-avatar"
                   />
                   <div className="booking-screen-details">
                     <h1 className="booking-screen-author1">{drName}</h1>
                     <label className="booking-screen-position">
-                      {drId}
+                      {specality}
                     </label>
                     <div className="booking-screen-author2">
                       <a
@@ -46,7 +92,6 @@ const BookingScreen = (props) => {
                             className="booking-screen-image"
                           />
                           <label className="booking-screen-position1">
-                            Pune
                           </label>
                         </div>
                       </a>
@@ -82,7 +127,7 @@ const BookingScreen = (props) => {
         </div>
       </section>
     </div>
+    
   )
 }
-
 export default BookingScreen
--- src/views/sign-up.js
+++ src/views/sign-up.js
@@ -1,14 +1,75 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import { Helmet } from 'react-helmet'
-
+import axios from "axios";
 import './sign-up.css'
-
+var finalFirstName= "";
+var finalLastName= "";
 const SignUp = (props) => {
   var drId =localStorage.getItem('drId');
   var drName =localStorage.getItem('drName');
   var specality =localStorage.getItem('specality');
+  var mobileNumber =localStorage.getItem('mobileNumber');
+  var photourl="http://68.183.83.230:8765/provider-service/doctor/" + drId+ "/photo"
+
+  //for work
+  const history = useHistory();
+
+  const signUp = async () => {
+    if (finalFirstName.length !=null&&finalLastName!=null&&mobileNumber!=null) {
+      const payload = {
+      firstName: finalFirstName,
+      lastName: finalLastName,
+      mobileNumber: mobileNumber,
+      requestedRole:"PATIENT",
+      preferredLanguageId:"5ad887231fd47a000d03368d"
+      }
+      const responce = await axios({
+        method: "post",
+        // url: `https://api.qupdev.com/userauth/users/sign-up` ,
+        url: `http://68.183.83.230:8765/userauth/users/sign-up`,
+        data: payload,
+        headers: {
+          "Content-Type": "application/json",
+           'Authorization': 'Basic '+btoa('qup-mobile:mob@46$qup'),
+        },
+      }).then((responce) => {      
+        history.push("/login");
+        alert("Sign UP Succesfull \nNow Login With Mobile number")
+      }).catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          // console.log(error.response.data);
+          // console.log("Error api status",error.response.status);
+          console.log("ERROR",error.responce)
+          alert("Something Went Wrong Try Again")
+        
+          // console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
+
+    } else {
+      alert("required 10 digits");
+    }
+  };
+
+  const handelFirstName = (event) => {
+   finalFirstName = event.target.value;
+  };
+  const handelLastName = (event) => {
+   finalLastName = event.target.value;
+  };
   return (
     <div className="sign-up-container">
       <Helmet>
@@ -21,7 +82,7 @@ const SignUp = (props) => {
             <main className="sign-up-main">
               <img
                 alt="image"
-                src="https://max-website20-images.s3.ap-south-1.amazonaws.com/Dr_Puneet_sq_3be6907e0c.jpg"
+                src={photourl}
                 className="sign-up-image"
               />
               <header className="home-header">
@@ -29,11 +90,15 @@ const SignUp = (props) => {
                 <span className="home-caption">
                   {specality}
                 </span>
+                <span className="home-caption">
+                  {mobileNumber}
+                </span>
                 <input
                   type="text"
                   name="First Name"
                   required
                   autoFocus
+                  onChange={handelFirstName}
                   placeholder="First Name"
                   autoComplete="on"
                   className="sign-up-textinput input"
@@ -43,19 +108,18 @@ const SignUp = (props) => {
                   name="Last Name"
                   required
                   placeholder="Last Name"
+                  onChange={handelLastName}
                   autoComplete="on"
                   className="sign-up-textinput1 input"
                 />
               </header>
               <div className="sign-up-buttons">
-                <Link to="/booking-screen" className="sign-up-navlink">
-                  <div className="sign-up-get-started button">
+                  <div onClick={signUp} className="sign-up-get-started button">
                     <span className="sign-up-text">
                       <span>Next</span>
                       <br></br>
                     </span>
                   </div>
-                </Link>
               </div>
               <span className="sign-up-caption1">Powered by Q UP</span>
             </main>
